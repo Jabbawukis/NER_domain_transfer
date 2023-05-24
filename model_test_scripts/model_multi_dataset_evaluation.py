68b7fe6ca646dee99025461fc81be5de9be02883
daniel.christoph <daniel.christoph@open5g-test.com>
2023-05-24 21:39:44 +0200

    add data set

--- model_test_scripts/model_multi_dataset_evaluation.py
+++ model_test_scripts/model_multi_dataset_evaluation.py
@@ -2,9 +2,9 @@ import sys
 from flair.models import SequenceTagger
 from flair.datasets import CONLL_03_GERMAN
 
-
 corpus = CONLL_03_GERMAN(base_path="CONLL_03_GER")
 roberta_tagger = SequenceTagger.load(sys.argv[1])
-roberta_tagger.evaluate(data_points=corpus.test,
-                        gold_label_type="ner",
-                        out_path=sys.argv[2])
+test_results = roberta_tagger.evaluate(data_points=corpus.test,
+                                       gold_label_type="ner",
+                                       out_path=sys.argv[2])
+print(test_results.main_score)
