a0f565e9c0d005ba9d5a6314b5b3f790a8e85252
daniel.christoph <daniel.christoph@open5g-test.com>
2023-05-24 21:37:30 +0200

    add data set

--- model_test_scripts/model_multi_dataset_evaluation.py
+++ model_test_scripts/model_multi_dataset_evaluation.py
@@ -7,4 +7,4 @@ corpus = CONLL_03_GERMAN(base_path="CONLL_03_GER")
 roberta_tagger = SequenceTagger.load(sys.argv[1])
 roberta_tagger.evaluate(data_points=corpus.test,
                         gold_label_type="ner",
-                        out_path=".")
+                        out_path=sys.argv[2])
