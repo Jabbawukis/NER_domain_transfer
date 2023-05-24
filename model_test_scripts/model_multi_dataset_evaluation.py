9172c961bdf540ad8c97ab344bad4c10049cfaaf
daniel.christoph <daniel.christoph@open5g-test.com>
2023-05-24 21:16:50 +0200

    add data set

--- model_test_scripts/model_multi_dataset_evaluation.py
+++ model_test_scripts/model_multi_dataset_evaluation.py
@@ -6,5 +6,5 @@ from flair.datasets import CONLL_03_GERMAN
 corpus = CONLL_03_GERMAN(base_path="CONLL_03_GER")
 roberta_tagger = Classifier.load(sys.argv[1])
 roberta_tagger.evaluate(data_points=corpus.test,
-                        gold_label_type="tag_type",
+                        gold_label_type="ner",
                         out_path=".")
