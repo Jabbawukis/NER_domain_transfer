3b2d5342ebfc9f5ba64df646d25ba1c833a97bf4
daniel.christoph <daniel.christoph@open5g-test.com>
2023-06-04 22:04:44 +0200

    add bit fit training scripts

--- model_test_scripts/model_multi_dataset_evaluation.py
+++ model_test_scripts/model_multi_dataset_evaluation.py
@@ -1,7 +1,9 @@
 import sys
+
+import flair
 from flair.models import SequenceTagger
 from flair.datasets import CONLL_03_GERMAN
-
+flair.device = f'cuda:{sys.argv[3]}'
 corpus = CONLL_03_GERMAN(base_path="CONLL_03_GER")
 roberta_tagger = SequenceTagger.load(sys.argv[1])
 test_results = roberta_tagger.evaluate(data_points=corpus.test,
