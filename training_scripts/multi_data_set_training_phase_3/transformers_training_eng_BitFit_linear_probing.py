bb4cbf6537b6cdc02d4004b2b021b7fdc6823afd
daniel.christoph <daniel.christoph@open5g-test.com>
2023-06-08 15:19:09 +0200

    refactor

--- training_scripts/multi_data_set_training_phase_3/transformers_training_eng_BitFit_linear_probing.py
+++ training_scripts/multi_data_set_training_phase_3/transformers_training_eng_BitFit_linear_probing.py
@@ -10,7 +10,7 @@ flair.device = f'cuda:{sys.argv[1]}'
 corpus = CONLL_03()
 label_type = 'ner'
 label_dict = corpus.make_label_dictionary(label_type=label_type)
-
+lr = "0_3"
 for run in range(1, 4):
     embeddings = TransformerWordEmbeddings(model='xlm-roberta-large',
                                            layers="-1",
@@ -29,21 +29,21 @@ for run in range(1, 4):
 
     trainer = ModelTrainer(tagger, corpus)
 
-    trainer.fine_tune(f'resources/taggers/conll_eng_ner_roberta_large_run_{run}_linear_probing',
-                      learning_rate=0.8,
+    trainer.fine_tune(f'resources/taggers/conll_eng_ner_roberta_large_run_{run}_linear_probing_lr{lr}',
+                      learning_rate=0.3,
                       mini_batch_size=32,
                       )
     del tagger
     del trainer
     del embeddings
 
-    finished_tagger = SequenceTagger.load(f'resources/taggers/conll_eng_ner_roberta_large_run_{run}_linear_probing/final-model.pt')
+    finished_tagger = SequenceTagger.load(f'resources/taggers/conll_eng_ner_roberta_large_run_{run}_linear_probing_lr{lr}/final-model.pt')
     finished_tagger.embeddings.fine_tune = True
     finished_tagger.embeddings.static_embeddings = False
     optimizer = AdamW([param for name, param in finished_tagger.named_parameters() if name.endswith(".bias")])
 
     new_trainer = ModelTrainer(finished_tagger, corpus)
-    new_trainer.fine_tune(f'resources/taggers/conll_eng_ner_roberta_large_run_{run}_full_fine_tuning_BitFit',
+    new_trainer.fine_tune(f'resources/taggers/conll_eng_ner_roberta_large_run_{run}_full_fine_tuning_BitFit_lr{lr}',
                           learning_rate=5.0e-6,
                           mini_batch_size=4,
                           optimizer=optimizer
