05b73a96fae08a9a5024781f425376ffb6cec738
daniel.christoph <daniel.christoph@open5g-test.com>
2023-06-20 20:37:15 +0200

    switch to train but with fine tune parameters

--- training_scripts/multi_data_set_training_phase_4/transformers_training_eng.py
+++ training_scripts/multi_data_set_training_phase_4/transformers_training_eng.py
@@ -4,6 +4,9 @@ from flair.datasets import CONLL_03
 from flair.embeddings import TransformerWordEmbeddings
 from flair.models import SequenceTagger
 from flair.trainers import ModelTrainer
+import torch
+from flair.optim import LinearSchedulerWithWarmup
+
 flair.device = f'cuda:{sys.argv[1]}'
 corpus = CONLL_03(base_path="CONLL_03_ENG_GER_combination")
 label_type = 'ner'
@@ -26,12 +29,19 @@ for run in range(1, 4):
 
     trainer = ModelTrainer(tagger, corpus)
 
-    trainer.fine_tune(f'resources/taggers/conll_eng_ner_roberta_large_run_{run}_ger_test_as_dev',
-                      learning_rate=5.0e-6,
-                      mini_batch_size=4,
-                      **{"monitor_test": True,
-                         "monitor_dev": True}
-                      )
+    trainer.train(base_path=f'resources/taggers/conll_eng_ner_roberta_large_run_{run}_ger_test_as_dev',
+                  learning_rate=5.0e-6,
+                  max_epochs=10,
+                  optimizer=torch.optim.AdamW,
+                  scheduler=LinearSchedulerWithWarmup,
+                  warmup_fraction=0.1,
+                  mini_batch_size=4,
+                  embeddings_storage_mode="none",
+                  use_final_model_for_eval=True,
+                  decoder_lr_factor=1.0,
+                  monitor_test=True,
+                  monitor_dev=True,
+                  )
     del tagger
     del trainer
     del embeddings
