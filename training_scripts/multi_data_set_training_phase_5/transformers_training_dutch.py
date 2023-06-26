ad501e89e7fbda096c2566cb010aa994fe4a2304
daniel.christoph <daniel.christoph@open5g-test.com>
2023-06-26 22:57:26 +0200

    add scripts for phase 3

---
+++ training_scripts/multi_data_set_training_phase_5/transformers_training_dutch.py
@@ -0,0 +1,46 @@
+import sys
+import flair
+from flair.datasets import CONLL_03_DUTCH, CONLL_03_GERMAN
+from flair.embeddings import TransformerWordEmbeddings
+from flair.models import SequenceTagger
+from flair.trainers import ModelTrainer
+import torch
+from flair.optim import LinearSchedulerWithWarmup
+
+flair.device = f'cuda:{sys.argv[1]}'
+corpus = CONLL_03_DUTCH()
+corpus._dev = CONLL_03_GERMAN(base_path="../../model_test_scripts/CONLL_03_GER").test
+label_type = 'ner'
+label_dict = corpus.make_label_dictionary(label_type=label_type)
+for run in range(1, 4):
+    embeddings = TransformerWordEmbeddings(model='xlm-roberta-large',
+                                           layers="-1",
+                                           subtoken_pooling="first",
+                                           fine_tune=True,
+                                           use_context=True,
+                                           )
+    tagger = SequenceTagger(hidden_size=256,
+                            embeddings=embeddings,
+                            tag_dictionary=label_dict,
+                            tag_type=label_type,
+                            use_crf=False,
+                            use_rnn=False,
+                            reproject_embeddings=False,
+                            )
+
+    trainer = ModelTrainer(tagger, corpus)
+
+    trainer.train(base_path=f'resources/taggers/conll_dutch_ner_roberta_large_run_{run}_ger_test_as_dev',
+                  learning_rate=5.0e-6,
+                  max_epochs=10,
+                  optimizer=torch.optim.AdamW,
+                  scheduler=LinearSchedulerWithWarmup,
+                  warmup_fraction=0.1,
+                  mini_batch_size=4,
+                  embeddings_storage_mode="none",
+                  use_final_model_for_eval=True,
+                  monitor_test=True,
+                  )
+    del tagger
+    del trainer
+    del embeddings
