46578c65d7e36b91a9ae15d73d3ac76a9aae0fc6
daniel.christoph <daniel.christoph@open5g-test.com>
2023-07-22 19:29:33 +0200

    add training script

---
+++ training_scripts/multi_data_set_training_phase_6/transformers_training_eng_dutch_best_combination.py
@@ -0,0 +1,49 @@
+import sys
+import flair
+from flair.data import MultiCorpus
+from flair.datasets import CONLL_03, CONLL_03_GERMAN, CONLL_03_DUTCH
+from flair.embeddings import TransformerWordEmbeddings
+from flair.models import SequenceTagger
+from flair.trainers import ModelTrainer
+
+flair.device = f'cuda:{sys.argv[1]}'
+corpus = MultiCorpus([CONLL_03_DUTCH(), CONLL_03()])
+corpus._dev = CONLL_03_GERMAN(base_path="../../model_test_scripts/CONLL_03_GER").test
+label_type = 'ner'
+label_dict = corpus.make_label_dictionary(label_type=label_type)
+
+combi_1 = [400, 9e-05]
+
+for combination in [combi_1]:
+    for run in range(1, 4):
+        mini_batch = combination[0]
+        lr = combination[1]
+
+        embeddings = TransformerWordEmbeddings(model='xlm-roberta-large',
+                                               layers="-1",
+                                               subtoken_pooling="first",
+                                               fine_tune=True,
+                                               use_context=True,
+                                               )
+        tagger = SequenceTagger(hidden_size=256,
+                                embeddings=embeddings,
+                                tag_dictionary=label_dict,
+                                tag_type=label_type,
+                                use_crf=False,
+                                use_rnn=False,
+                                reproject_embeddings=False,
+                                )
+
+        trainer = ModelTrainer(tagger, corpus)
+
+        trainer.fine_tune(
+            base_path=f'resources/taggers/conll_eng_dutch_ner_roberta_large_mini_batch_{mini_batch}_lr_{lr}_run_{run}_ger_test_as_dev',
+            learning_rate=lr,
+            max_epochs=10,
+            mini_batch_size=mini_batch,
+            monitor_test=True,
+            mini_batch_chunk_size=4,
+            )
+        del tagger
+        del trainer
+        del embeddings
